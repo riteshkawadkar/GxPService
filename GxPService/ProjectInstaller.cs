0e0f1e2e55783adc8db1949a8ca76f7246545ef3
Ritesh Kawadkar <ritesh.kawadkar@gmail.com>
2024-02-27 21:34:16 +0530

    Add project files.

---
+++ GxPService/ProjectInstaller.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration.Install;
+using System.Linq;
+
+namespace GxPService
+{
+    [RunInstaller(true)]
+    public partial class ProjectInstaller : System.Configuration.Install.Installer
+    {
+        public ProjectInstaller()
+        {
+            //Don't ever try to call any method before the call of the InitializeComponent process.
+            InitializeComponent();
+        }
+    }
+}
